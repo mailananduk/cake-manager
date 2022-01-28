cb8e37992b7a62a1cbc9dfb2e7c6630aa2daa59f
Anand Chetty <mailanand.k@gmail.com>
2022-01-28 05:40:28 +0000

    Integration test, test for Authorization

--- src/test/java/com/wracle/cakemgr/cakemanager/ui/controller/CakeControllerIntegrationTest.java
+++ src/test/java/com/wracle/cakemgr/cakemanager/ui/controller/CakeControllerIntegrationTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wracle.cakemgr.cakemanager.io.entity.CakeEntity;
 import com.wracle.cakemgr.cakemanager.io.repository.CakeRepository;
 import com.wracle.cakemgr.cakemanager.ui.model.request.CakeRequestModel;
+import com.wracle.cakemgr.cakemanager.ui.model.request.UsernamePasswordLoginRequestModel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -33,15 +35,26 @@ public class CakeControllerIntegrationTest {
 
     @Test
     public void cakeAllLayers() throws Exception {
-        CakeRequestModel cakeRequestModel = buildCakeRequestModel();
+        //login to get Authorization (in response header)
+        UsernamePasswordLoginRequestModel loginUser = buildLoginUser();
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/users/login")
+                        .content(objectMapper.writeValueAsString(loginUser))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andReturn();
+        String authorization = mvcResult.getResponse().getHeader("Authorization");
 
+        //use Authorization when calling create cake
+        CakeRequestModel cakeRequestModel = buildCakeRequestModel();
         mockMvc.perform(MockMvcRequestBuilders
-                .post("/cakes/")
-                .content(objectMapper.writeValueAsString(cakeRequestModel))
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", new String("Bearer: eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmFuZEBhbmFuZC5jb20iLCJleHAiOjE2NDM0MTE4MTJ9.3wO8zZ0_zFmC-cE4SOXkGco6n2Fb99au1o2W9zXoBhxeZoC2jVzoPJfMHQSHc26VKaSWBF9aYOUL6IsSxdnqYQ"))
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
+                        .post("/cakes/")
+                        .content(objectMapper.writeValueAsString(cakeRequestModel))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", authorization)
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isCreated())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Lemon Cake 100"));
 
         Optional<CakeEntity> cakeEntityOptional  = cakeRepository.findByName("Lemon Cake 100");
@@ -51,6 +64,24 @@ public class CakeControllerIntegrationTest {
         Assertions.assertEquals("www.mycake.com/img100", cakeEntity.getImageUrl());
     }
 
+    @Test
+    public void cakeAllLayersWithoutAuthorization() throws Exception {
+        CakeRequestModel cakeRequestModel = buildCakeRequestModel();
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/cakes/")
+                        .content(objectMapper.writeValueAsString(cakeRequestModel))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+
+    private UsernamePasswordLoginRequestModel buildLoginUser() {
+        UsernamePasswordLoginRequestModel loginUser = new UsernamePasswordLoginRequestModel();
+        loginUser.setEmailId("anand@anand.com");
+        loginUser.setPassword("admin");
+        return loginUser;
+    }
+
     private CakeRequestModel buildCakeRequestModel() {
         CakeRequestModel cakeRequestModel = new CakeRequestModel();
         cakeRequestModel.setName("Lemon Cake 100");
@@ -58,4 +89,5 @@ public class CakeControllerIntegrationTest {
         cakeRequestModel.setImageUrl("www.mycake.com/img100");
         return cakeRequestModel;
     }
+
 }
